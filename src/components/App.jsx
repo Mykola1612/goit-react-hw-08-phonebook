049cf6d7438200dacad94391ce7b28b93dde56e1
Mykola1612 <fermer2007pavlovich@gmail.com>
2023-12-04 22:24:19 +0100

    Summary

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,19 @@
-import ContactsPage from 'pages/ContactsPage';
-import HomePage from 'pages/HomePage';
-import LoginPage from 'pages/LoginPage';
-import RegisterPage from 'pages/RegisterPage';
-import { useEffect } from 'react';
+// import ContactsPage from 'pages/ContactsPage';
+// import HomePage from 'pages/HomePage';
+// import LoginPage from 'pages/LoginPage';
+// import RegisterPage from 'pages/RegisterPage';
+import { lazy, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { refreshThunk } from 'redux/auth/auth.reducer';
 import Layout from './Layout/Layout';
 import RestrictedRoute from './RestrictedRoute';
 import PrivateRoute from './PrivateRoute';
+
+const HomePage = lazy(() => import('pages/HomePage'));
+const LoginPage = lazy(() => import('pages/LoginPage'));
+const RegisterPage = lazy(() => import('pages/RegisterPage'));
+const ContactsPage = lazy(() => import('pages/ContactsPage'));
 
 export const App = () => {
   const dispatch = useDispatch();
